f91c73c6ff3c0dbf958ee56925d58fc2fea61f2a
developerkerimzade@gmail.com <Arsenalozil1997>
2023-01-31 13:37:04 +0500

    31.01.2022

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -4,21 +4,33 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] array = {346,34686,17452,12,1,90,200,250,1200,1000};
+        int[] array = {346, 34686, 17452, 12, 1, 90, 200, 250, 1200, 1000};
 //       bubbleSort(array);
 //        findMinIndexOfMassive(array);
-        sortBubble(array);
+ //       sortBubble(array);
+
+
+        for (int step = 0; step < array.length; step++){
+            System.out.println(Arrays.toString(array));
+
+            int index  =  selectionSort(array,step);
+
+            int tmp = array[step];
+            array[step] = array[index];
+            array[index] = tmp;
+
+        }
     }
 
 
-    public static void bubbleSort(int [] array) {
-        for (int i =  array.length -1; i>=0; i--){
+    public static void bubbleSort(int[] array) {
+        for (int i = array.length - 1; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
 
-                if (array[j] >array[j+1]){
+                if (array[j] > array[j + 1]) {
                     int t = array[j];
-                    array[i ] =array[j+1];
-                    array[j +1] = t;
+                    array[i] = array[j + 1];
+                    array[j + 1] = t;
                 }
 
             }
@@ -26,35 +38,47 @@ public class Main {
         System.out.println(Arrays.toString(array));
     }
 
-    public static void findMinIndexOfMassive(int [] array) {
+    public static void findMinIndexOfMassive(int[] array) {
         int minValue = array[0];
         int minIndex = 0;
         for (int i = 1; i < array.length; i++) {
-            if (array[i] < minValue){
+            if (array[i] < minValue) {
                 minValue = array[i];
-                minIndex= i;
+                minIndex = i;
             }
         }
         System.out.println(minValue + " min value");
-        System.out.println(minIndex  + " min index");
+        System.out.println(minIndex + " min index");
     }
 
 
-    public static void sortBubble(int [] array){
+    public static void sortBubble(int[] array) {
 
         boolean isSorted = false;
         while (!isSorted) {
             isSorted = true;
-            for (int i =1; i < array.length; i++){
-                if (array[i] < array[i-1]) {
-                    int temp =  array[i];
-                    array[i] = array[i-1];
-                    array[i-1] = temp;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
 
-                    isSorted =false;
+                    isSorted = false;
                 }
             }
             System.out.println(Arrays.toString(array));
         }
+    }
+
+    private static int selectionSort(int[] array, int start) {
+        int minIndex = start;
+        int minValue = array[start];
+        for (int i = start + 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+                minIndex = i;
+            }
+        }
+return minIndex;
     }
 }
